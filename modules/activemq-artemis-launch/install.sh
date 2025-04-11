a777e4dd8633308029b1e42226cc244775670f51
Domenico Francesco Bruscino <brusdev@apache.org>
2025-04-11 10:09:02 +0200

    [#119] Update keycloak version to 24
    (cherry picked from commit 70afec6334320ce1027eaf3411748fbc44574c12)
    
    downstream: ENTMQBR-9701

--- modules/activemq-artemis-launch/install.sh
+++ modules/activemq-artemis-launch/install.sh
@@ -18,20 +18,21 @@ if ! ls $AMQ_HOME/web/metrics.war; then
 fi
 
 if ! ls $AMQ_HOME/lib/keycloak-adapter-core*.jar; then
-  curl -L --output "$AMQ_HOME/lib/bcprov-jdk15on-1.70.jar" https://repo.maven.apache.org/maven2/org/bouncycastle/bcprov-jdk15on/1.70/bcprov-jdk15on-1.70.jar
-  curl -L --output "$AMQ_HOME/lib/bcpkix-jdk15on-1.70.jar" https://repo.maven.apache.org/maven2/org/bouncycastle/bcpkix-jdk15on/1.70/bcpkix-jdk15on-1.70.jar
-  curl -L --output "$AMQ_HOME/lib/commons-codec-1.15.jar" https://repo.maven.apache.org/maven2/commons-codec/commons-codec/1.15/commons-codec-1.15.jar
-  curl -L --output "$AMQ_HOME/lib/commons-logging-1.2.jar" https://repo.maven.apache.org/maven2/commons-logging/commons-logging/1.2/commons-logging-1.2.jar
-  curl -L --output "$AMQ_HOME/lib/httpclient-4.5.13.jar" https://repo.maven.apache.org/maven2/org/apache/httpcomponents/httpclient/4.5.13/httpclient-4.5.13.jar
+  curl -L --output "$AMQ_HOME/lib/bcprov-jdk18on-1.80.jar" https://repo.maven.apache.org/maven2/org/bouncycastle/bcprov-jdk18on/1.80/bcprov-jdk18on-1.80.jar
+  curl -L --output "$AMQ_HOME/lib/commons-codec-1.18.0.jar" https://repo.maven.apache.org/maven2/commons-codec/commons-codec/1.18.0/commons-codec-1.18.0.jar
+  curl -L --output "$AMQ_HOME/lib/httpclient-4.5.14.jar" https://repo.maven.apache.org/maven2/org/apache/httpcomponents/httpclient/4.5.14/httpclient-4.5.14.jar
   curl -L --output "$AMQ_HOME/lib/httpcore-4.4.16.jar" https://repo.maven.apache.org/maven2/org/apache/httpcomponents/httpcore/4.4.16/httpcore-4.4.16.jar
-  curl -L --output "$AMQ_HOME/lib/jackson-annotations-2.14.2.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar
-  curl -L --output "$AMQ_HOME/lib/jackson-core-2.14.2.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-core/2.14.2/jackson-core-2.14.2.jar
-  curl -L --output "$AMQ_HOME/lib/jackson-databind-2.14.2.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.14.2/jackson-databind-2.14.2.jar
+  curl -L --output "$AMQ_HOME/lib/jackson-annotations-2.18.3.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.18.3/jackson-annotations-2.18.3.jar
+  curl -L --output "$AMQ_HOME/lib/jackson-core-2.18.3.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-core/2.18.3/jackson-core-2.18.3.jar
+  curl -L --output "$AMQ_HOME/lib/jackson-databind-2.18.3.jar" https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.18.3/jackson-databind-2.18.3.jar
   curl -L --output "$AMQ_HOME/lib/jakarta.activation-1.2.2.jar" https://repo.maven.apache.org/maven2/com/sun/activation/jakarta.activation/1.2.2/jakarta.activation-1.2.2.jar
-  curl -L --output "$AMQ_HOME/lib/jboss-logging-3.5.0.Final.jar" https://repo.maven.apache.org/maven2/org/jboss/logging/jboss-logging/3.5.0.Final/jboss-logging-3.5.0.Final.jar
-  curl -L --output "$AMQ_HOME/lib/keycloak-adapter-core-18.0.2.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-adapter-core/18.0.2/keycloak-adapter-core-18.0.2.jar
-  curl -L --output "$AMQ_HOME/lib/keycloak-common-18.0.2.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-common/18.0.2/keycloak-common-18.0.2.jar
-  curl -L --output "$AMQ_HOME/lib/keycloak-core-18.0.2.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-core/18.0.2/keycloak-core-18.0.2.jar
+  curl -L --output "$AMQ_HOME/lib/jboss-logging-3.5.3.Final.jar" https://repo.maven.apache.org/maven2/org/jboss/logging/jboss-logging/3.5.3.Final/jboss-logging-3.5.3.Final.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-adapter-core-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-adapter-core/24.0.5/keycloak-adapter-core-24.0.5.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-common-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-common/24.0.5/keycloak-common-24.0.5.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-core-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-core/24.0.5/keycloak-core-24.0.5.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-crypto-default-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-crypto-default/24.0.5/keycloak-crypto-default-24.0.5.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-server-spi-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-server-spi/24.0.5/keycloak-server-spi-24.0.5.jar
+  curl -L --output "$AMQ_HOME/lib/keycloak-server-spi-private-24.0.5.jar" https://repo.maven.apache.org/maven2/org/keycloak/keycloak-server-spi-private/24.0.5/keycloak-server-spi-private-24.0.5.jar
 fi
 
 cp -p ${SOURCES_DIR}/netty-tcnative*.jar \
